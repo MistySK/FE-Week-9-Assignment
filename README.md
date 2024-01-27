4d025a5b6f43f3d8566c6f016782b9cca4b136a9
Misty <misty@MAC058.local>
2024-01-27 12:07:38 -0500

    final coding

--- README.md
+++ README.md
@@ -17,3 +17,98 @@ Functions/methods?
 - add a point for the player with the higher card
 - play until all cards are played
 - return the winner or tie if no one wins
+
+--------------------- add back to js tab
+class Player {
+    constructor(playerNameValue) {
+        this.playerName = playerNameValue;
+        //need: player hand, points - 0 (+1), player name
+    }
+}
+
+class Game //start method, perform order of operations (deck class, deal, compare, points)
+
+//how to deal 26 cards to two players
+
+----------------------
+class Card {
+    constructor(suit, rank) {
+        this.suit = suit;
+        this.rank = rank;
+        //this.value = value; //is this needed since ranks are defined below?
+    }
+}
+
+/* class Deck {
+    constructor(suits, values) {
+        this.suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
+        this.ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        //is this needed? this.values = [2 - 14]
+    } //method for dealing cards & method for shuffling
+    //outer loop for length of suits; inner loop  - run 13x = 52 & pass in suits and values
+} */
+
+//Option from Evan's class:
+class Deck {
+    constructor() {
+        this.cards = [];
+        let suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
+        let ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+        for(let suitIndex = 0; suitIndex < suits.length; suitIndex++) { //suitIndex is used instead of i
+            for( let rankIndex = 0; rankIndex < ranks.length; rankIndex++) { //rankIndex is used instead of j
+                this.cards.push(new Card(suits[suitIndex], ranks[rankIndex]));
+        }
+    }
+}
+
+deal(playerOne, playerTwo) {
+
+}
+}
+
+let myDeck = new Deck();
+
+console.log(myDeck);
+
+deal(players) {
+    this.shuffle();
+    for (let i = 0; i < this.cards.length; i++) {
+        players[i % players.length].hand.push(this.cards[i]);
+    }
+}
+
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.hand = 0;
+        this.score = 0;
+    }
+} 
+
+class Game {
+    constructor() {
+    this.players = [new Player("Player 1"), new Player("Player 2")]
+    this.deck = new Deck();
+    this.deck.deal(this.players);
+}
+getPlayer1deck(){
+    return this.players[0].hand;
+}
+getPlayer2deck(){
+    return this.players[1].hand;
+}
+startGame() {
+    while (this.players[0].hand.length > 0 && this.players[1].hand.length > 0) {
+        this.playRound();
+    }
+    this.declareWinner();
+}
+
+// class Game //start method, perform order of operations (deck class, deal, compare, points)
+
+//how to deal 26 cards to two players
+
+myDeck.deal(deal(playerOne: any, playerTwo: any):void
+//call methods for playerOne and playerTwo to play and compare cards
--- scripts.js
+++ scripts.js
@@ -1,38 +1,122 @@
+//classes needed: card, deck, player, game
+
 class Card {
-    constructor (suit, rank) {
+    constructor(suit, rank) {
         this.suit = suit;
         this.rank = rank;
+        this.value = this.getValue(rank);
     }
-)    
-
-class Deck {
-    constructor(suits, values) {
-        this.suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
-        this.ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+    //define the values of each face card and then parse in the integer cards 
+    getValue(rank) {
+        if(rank === 'Ace')return 14;
+        if(rank === 'King')return 13;
+        if(rank === 'Queen')return 12;
+        if(rank === 'Jack')return 11;
+        return parseInt(rank);
     }
 }
 
-const cardRanks = {
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    '10': 10,
-    'J': 10,
-    'Q': 10,
-    'K': 10,
-    'A': 11 // you can also set it to 1 if needed
+
+class Deck {
+    constructor() {
+        this.cards = this.generateDeck();
+    }
+    generateDeck() {
+        const suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+        const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        const deck = [];
+        for(let i = 0; i < suits.length; i++) { //could use suitIndex instead of i
+            for(let j = 0; j < ranks.length; j++) { //could use rankIndex instead of j
+                deck.push(new Card(suits[i], ranks[j]));
+                }   
+        } 
+        return deck;
+    }
+    shuffle() {
+        //shuffle to ensure random cards; otherwise they would all be in order from 2-14?
+        for(let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+    deal(players){
+        //deal 26 cards to each player
+        this.shuffle();
+        for(let i = 0; i < this.cards.length; i++) {
+            players[i % players.length].hand.push(this.cards[i]);
+        }
+    }
 }
 
 class Player {
-    constructor(player) {
-        this.player = player;
+    constructor(name){
+        this.name = name;
+        this.hand = [];
+        this.score = 0;
+    }
+    playCard() {
+        return this.hand.shift();
     }
 }
 
-//how to deal 26 cards to two players
 
+let myDeck = new Deck();
+console.log(myDeck);
+
+
+class Game {
+    constructor() {
+    this.players = [new Player("Player 1"), new Player("Player 2")]
+    this.deck = new Deck();
+    this.deck.deal(this.players);
+}
+getPlayer1Deck(){
+    return this.players[0].hand;
+}
+getPlayer2Deck(){
+    return this.players[1].hand;
+}
+startGame() {
+    while (this.players[0].hand.length > 0 && this.players[1].hand.length > 0) {
+        this.playRound();
+    }
+    this.declareWinner();
+}
+playRound() {
+    // Draw a card from the each deck and add it to each player's hand
+    const card1 = this.players[0].playCard();
+    const card2 = this.players[1].playCard();
+
+    console.log(`${this.players[0].name} plays ${card1.rank} of ${card1.suit}`);
+    console.log(`${this.players[1].name} plays ${card2.rank} of ${card2.suit}`);
+
+    //compare the value of the cards in each hand/round
+    if (card1.value > card2.value) {
+        this.players[0].score++;
+        console.log(`${this.players[0].name} wins this round.`);
+    }   else if (card2.value > card1.value) {
+        this.players[1].score++;
+        console.log(`${this.players[1].name} wins this round.`);
+    }   else {
+        console.log('This round is a tie.');
+    }
+}
+
+declareWinner() {
+    if (this.players[0].score > this.players[1].score) {
+        console.log(
+            `${this.players[0].name} wins the game with a score of ${this.players[0].score}!`
+        );
+        }   else if (this.players[1].score > this.players[0].score) {
+            console.log(
+                `${this.players[1].name} wins the game with a score of ${this.players[1].score}!`
+            );
+    } else {
+        console.log('This game is a tie!');
+    }
+}
+}
+
+//Start game
+const warGame = new Game();
+warGame.startGame();
